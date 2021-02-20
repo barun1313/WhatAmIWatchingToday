bd759c2d1d787c370ea8fcbf0e6455da1444de88
Barun Patra <barunpatra1313@gmail.com>
2021-02-20 14:45:58 +0530

    Added Tournament Name, Schedule for AOE2 matches

--- gamelist.py
+++ gamelist.py
@@ -1,7 +1,4 @@
 import requests
-import psycopg2
-import json
-from time import sleep
 import bs4
 from datetime import datetime, timedelta
 
@@ -13,33 +10,51 @@ matchTables = soup.findAll(
     "table", {"class": "wikitable wikitable-striped infobox_matches_content"})
 
 for matchTable in matchTables:
-    substring = "(page does not exist)"
     if(matchTable.find('td', {"class": "team-left"})):
+        # finds the name of the tournament
+        tournamentName = matchTable.find('td', {"class": "match-filler bgc-lighter"}).find(
+            'div', {"class": "matchticker-tournament-wrapper"}).find('div', {"class": "matchticker-tournament-name"}).find('a').contents[0]
+
+        # caluclates time of the match (converts string to datetime) and then converts it into IST by adding 5.5 hours
+        matchTime = matchTable.find('td', {"class": "match-filler bgc-lighter"}).find(
+            'span', {"class": "timer-object timer-object-countdown-only"}).contents[0]
+        countdownUtc = datetime.strptime(
+            matchTime, '%B %d, %Y - %H:%M ')
+        countdownIst = countdownUtc + timedelta(hours=5.5)
+
+        # finds the players involved
         leftTeam = matchTable.find('td', {"class": "team-left"})
         rightTeam = matchTable.find('td', {"class": "team-right"})
+
         if(leftTeam.find('span', {"style": "white-space:pre"})):
             leftName = leftTeam.find(
                 'span', {"style": "white-space:pre"}).find('a')
+
             if leftName is None:
                 continue
             leftPlayer = leftName.contents[0]
             rightName = rightTeam.find(
                 'span', {"style": "white-space:pre"}).find('a')
+
             if rightName is None:
                 continue
             rightPlayer = rightName.contents[0]
             # countdown = matchTable.find('td', {"class": "match-filler bgc-lighter"}).find(
             #     'span', {"class": "timer-object timer-object-countdown-only"}).contents[0]
-            print(leftPlayer, "vs", rightPlayer)
+            print(leftPlayer, "vs", rightPlayer,
+                  ", Tournament Name :", tournamentName, "at", countdownIst)
 
         elif(leftTeam.find('span', {"class": "team-template-text"})):
+
+            tournamentName = matchTable.find('td', {"class": "match-filler bgc-lighter"}).find(
+                'div', {"class": "matchticker-tournament-wrapper"}).find('div', {"class": "matchticker-tournament-name"}).find('a').contents[0]
+
             matchTime = matchTable.find('td', {"class": "match-filler bgc-lighter"}).find(
                 'span', {"class": "timer-object timer-object-countdown-only"}).contents[0]
             countdownUtc = datetime.strptime(
                 matchTime, '%B %d, %Y - %H:%M ')
             countdownIst = countdownUtc + timedelta(hours=5.5)
-            print(matchTime, countdownIst)
-            break
+
             leftName = leftTeam.find(
                 'span', {"class": "team-template-text"}).find('a')
             if leftName is None:
@@ -51,6 +66,7 @@ for matchTable in matchTables:
                 continue
             rightPlayer = rightName.contents[0]
 
-            print(leftPlayer, "vs", rightPlayer)
+            print(leftPlayer, "vs", rightPlayer,
+                  ", Tournament Name :", tournamentName, "at", countdownIst)
     else:
         continue
